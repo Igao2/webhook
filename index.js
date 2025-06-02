fcaa7bea339e0cf544ad12c4dd2e38c1010c16f3
Igor Costa <91141990+Igao2@users.noreply.github.com>
2025-06-02 16:55:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
+app.use(express.json()); // Middleware para ler JSON no corpo da requisição
+app.use(express.urlencoded({ extended: true })); // Middleware para ler dados de formulário (POST x-www-form-urlencoded)
 
-const port = process.env.PORT || 8000;
+// Rota para receber qualquer método (GET, POST, etc.)
+app.all('/receber', (req, res) => {
+    let response = '';
 
-app.use(express.json())
+    // Exibir os parâmetros GET
+    if (Object.keys(req.query).length > 0) {
+        response += 'GET Parameters:\n';
+        response += JSON.stringify(req.query, null, 2) + '\n';
+    }
 
-app.post("/receber",(req,res)=>{
-    var b = req.body;
-    res.status(200).send("Recebido")
-    return b;
-    
-})
-app.listen(port,()=>{
+    // Verificar e processar o corpo JSON da requisição
+    if (req.is('application/json') && Object.keys(req.body).length > 0) {
+        response += 'JSON Body:\n';
+        response += JSON.stringify(req.body, null, 2) + '\n';
+    } else if (Object.keys(req.body).length > 0) {
+        // Caso não seja um JSON, exibir os parâmetros POST (form-data ou x-www-form-urlencoded)
+        response += 'POST Parameters:\n';
+        response += JSON.stringify(req.body, null, 2) + '\n';
+    }
 
-})
+    res.type('text/plain').send(response || 'No parameters received.');
+});
+
+// Iniciar servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
